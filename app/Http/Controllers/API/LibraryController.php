76325b1258d324f22101870ce8f7d8c6fc75dacf
ihor-nosatov <igornosatov@yahoo.com>
2020-07-01 17:40:54 +0300

    finish swagger api documentation

--- app/Http/Controllers/API/LibraryController.php
+++ app/Http/Controllers/API/LibraryController.php
@@ -3,14 +3,19 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Repositories\LibraryRepository;
 use App\Http\Requests\BookRequest;
-use Illuminate\Support\Facades\Storage;
-use Illuminate\Support\Facades\File;
-use App\Book;
+use Illuminate\Http\Request;
 
 class LibraryController extends Controller
 {
+    protected $libraryRepository;
+
+    public function __construct(LibraryRepository $libraryRepository)
+    {
+        $this->libraryRepository = $libraryRepository;
+    }
+
     /**
      * @OA\Get(
      *     path="/library",
@@ -87,35 +92,8 @@ class LibraryController extends Controller
      */
     public function index(Request $request)
     {
-        $sortBy = 'id';
-        $orderBy = 'desc';
-        $perPage = 3;
-
-        $books = Book::query();
-
-        if ($request->has('genre')) {
-            $books->where('genre', $request->genre);
-        }
-        if (request()->get('search')) {
-            $search = $request->get('search');
-            $books->where('title', 'like', '%' . $search . '%')->orWhere('author', 'like', '%' . $search . '%');
-        }
-        if ($request->has('orderBy')) {
-            $orderBy = $request->query('orderBy');
-        }
-        if ($request->has('sortBy')) {
-            $sortBy = $request->query('sortBy');
-        }
-        if ($request->has('perPage')) {
-            $perPage = $request->query('perPage');
-        }
-
-        $books = $books
-         ->paginate($perPage)
-         ->appends('genre', request('genre'))
-         ->appends('sortBy', request('sortBy'))
-         ->appends('orderBy', request('orderBy'));
-        return response()->json($books);
+        $books = $this->libraryRepository->allBooks($request);
+        return  $books;
     }
 
     /**
@@ -180,21 +158,10 @@ class LibraryController extends Controller
      * )
      */
 
-    public function store(Request $request)
+    public function store(BookRequest $request)
     {
-        $cover = $request->file('image');
-        $extension = $cover->getClientOriginalExtension();
-        Storage::disk('public')->put($cover->getFilename().'.'.$extension, File::get($cover));
-    
-        $book = new Book();
-        $book->title = $request->title;
-        $book->author = $request->author;
-        $book->description = $request->description;
-        $book->genre = $request->author;
-        $book->image = $cover->getFilename().'.'.$extension;
-        $book->save();
-    
-        return response()->json('The book successfully added');
+        $book = $this->libraryRepository->storeBook($request);
+        return  $book;
     }
 
 
@@ -236,8 +203,8 @@ class LibraryController extends Controller
 
     public function edit($id)
     {
-        $book = Book::find($id);
-        return response()->json($book);
+        $book = $this->libraryRepository->editBook($id);
+        return  $book;
     }
 
         /**
@@ -306,30 +273,10 @@ class LibraryController extends Controller
      *     )
      * )
      */
-    public function update($id, Request $request)
+    public function update($id, BookRequest $request)
     {
-        $book = [
-            'title' => $request->title,
-            'author' => $request->author,
-            'description' => $request->description,
-            'genre' => $request->genre
-         ];
- 
-        if ($files = $request->file('image')) {
-            $destinationPath = 'uploads/';
-            $extension = date('YmdHis') . "." . $files->getClientOriginalExtension();
-            $files->move($destinationPath, $extension);
-            $book['image'] = "$extension";
-        }
-
-        $book['title'] = $request->get('title');
-        $book['author'] = $request->get('author');
-        $book['description'] = $request->get('description');
-        $book['genre'] = $request->get('genre');
- 
-        Book::where('id', $id)->update($book);
-
-        return response()->json('The book successfully updated');
+        $book = $this->libraryRepository->updateBook($request, $id);
+        return  $book;
     }
 
     /**
@@ -370,7 +317,7 @@ class LibraryController extends Controller
      */
     public function destroy($id)
     {
-        $book = Book::find($id)->delete();
-        return response()->json('The book successfully deleted');
+        $book = $this->libraryRepository->deleteBook($id);
+        return  $book;
     }
 }
